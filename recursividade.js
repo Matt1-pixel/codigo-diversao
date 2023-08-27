60d13dce46beb57a276119c9ffc2a1291478d50e
Matt-pixel <109974408+Matt1-pixel@users.noreply.github.com>
2023-08-27 16:31:03 -0300

    Update recursividade.js

--- recursividade.js
+++ recursividade.js
@@ -1,5 +1,6 @@
+//cria uma array onde sera armazenado os valores
 let arr1 = [];
-
+//inicia a variavel chamando um prompt dentro de uma cadeia condicional 
 function conc() {
   if (arr1.length < 5) {
     let x = prompt('Insira um número:');
@@ -9,7 +10,7 @@ function conc() {
         alert('Operação cancelada, tente novamente.')
     } else {
       let e = Number(x);
-      
+      //apos verificar se um numero foi inserido, da um .push na array e mostra no console
       if (!isNaN(e)) {
         arr1.push(e);
         console.log(arr1);
@@ -17,11 +18,12 @@ function conc() {
         console.log('Valor inválido. Insira um número válido.');
       }
     }
-    
+    //quando valor for atingido ele finaliza o loop
     if (arr1.length === 5) {
       console.log('Finalizado');
     } else {
       conc();
+//senão, chama a função fazendo a recursividade
     }
   }
 }
